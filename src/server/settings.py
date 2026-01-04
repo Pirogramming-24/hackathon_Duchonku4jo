367af058b23bdda78980d118644ba5bb4fac8575
bimvocado <labeou@naver.com>
2026-01-05 03:26:58 +0900

    Add EB domain to ALLOWED_HOSTS

--- src/server/settings.py
+++ src/server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-=-#82d%4v8j$g!bq$m%j_*7(gz08z6&r6+fauu1ielc#dg7)1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
