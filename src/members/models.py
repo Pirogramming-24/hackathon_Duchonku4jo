dfa63d06d3ea03a0ffbf6dd977a840a7a28ec10c
chrry03 <ocr925@dgu.ac.kr>
2026-01-05 17:47:55 +0900

    feat: 채령 members/models.py 완료

--- src/members/models.py
+++ src/members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.name
