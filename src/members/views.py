3aa73094b15243872e307de11874ae29dac893c5
chrry03 <ocr925@dgu.ac.kr>
2026-01-05 20:08:00 +0900

    feat: 채령 members/views.py 완료

--- src/members/views.py
+++ src/members/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+# members/views.py
+from django.views.decorators.http import require_POST
+#delete에서 사용자가 링크 입력해서 삭제하는걸 방지하기 위함
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Member
+from .forms import MemberForm
 
-# Create your views here.
+#멤버 목록 페이지
+#멤버 카드 목록만 보여줌
+def member_list(request):
+    members = Member.objects.all()
+
+    return render(request, 'members/member_list.html', {
+        'members': members,
+    })
+
+#멤버 생성 페이지
+#GET: 멤버 생성 폼 페이지
+#POST: 멤버 생성 처리
+def member_create(request):
+    if request.method == 'POST':
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('members:member_list')
+    else:
+        form = MemberForm()
+
+    return render(request, 'members/member_create.html', {
+        'form': form,
+    })
+
+#멤버 삭제
+@require_POST
+def member_delete(request, member_id):
+    member = get_object_or_404(Member, id=member_id)
+    member.delete()
+    return redirect('members:member_list')
