cd82d73e105d59ab4485fc16e96d703bc7a91045
chrry03 <ocr925@dgu.ac.kr>
2026-01-05 20:34:06 +0900

    feat: 채령 members/models.py 전공자여부 추가

--- src/members/models.py
+++ src/members/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
+
 class Member(models.Model):
     GENDER_CHOICES = [
-        ('M', 'Male'),
-        ('F', 'Female'),
+        ('M', '남자'),  # 남자
+        ('F', '여자'),  # 여자
     ]
 
-    name = models.CharField(max_length=20)
+    MAJOR_CHOICES = [
+        ('O', '전공자'),  # 전공자
+        ('X', '비전공자'),  # 비전공자
+    ]
+
+    name = models.CharField(max_length=30)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    major = models.CharField(max_length=1, choices=MAJOR_CHOICES)
 
     def __str__(self):
         return self.name
