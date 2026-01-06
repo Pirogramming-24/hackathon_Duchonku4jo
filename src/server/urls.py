6ca76e75481cad2985af821280caab7446aee48b
bimvocado <labeou@naver.com>
2026-01-06 16:44:44 +0900

    Remove reverse from root redirect

--- src/server/urls.py
+++ src/server/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda r: redirect('teams:team_home', week_id=1)),
+    path('', HttpResponseRedirect('/teams/1/')),  # 🔥 reverse 제거
     path('teams/', include('teams.urls')),
 ]
